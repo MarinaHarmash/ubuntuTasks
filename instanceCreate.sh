dd47a10475385a0ca89af2139f8952421203b812
Marina Bohdan <marina.garmash94.gmail.com>
2023-07-27 14:53:34 +0300

    create instance file

---
+++ instanceCreate.sh
@@ -0,0 +1,43 @@
+#!bin/bash
+
+set -ex;
+
+sudo apt update;
+
+sudo apt install nginx;
+
+sudo systemctl enable nginx
+
+sudo systemctl is-anabled nginx
+
+sudo mkdir -p /var/www/firstSite/html
+
+echo "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Main</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+    <h1>Hello, World</h1>
+</body>
+</html>" | sudo tee /var/www/firstSite/html/index.html
+
+echo "server {
+    listen 888;
+    listen [::]:888;
+
+    root /var/www/firstSite/html;
+    index index.html;
+}" | sudo tee /etx/nginx/sites-available/firstSite.conf 
+
+
+sudo ln -s /etc/nginx/sites-available/firstSite.conf /etc/nginx/sites-enabled/
+
+sudo nginx -t
+
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo systemctl restart nginx
+
+
